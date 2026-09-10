efcaaf5bcd9a875fbef2715d19a04edb8bccf444
Hans <hans@acalistudios.com>
2026-09-09 19:33:12 -0700

    chore: relocate Android native project to C:/Users/hansi/AndroidStudioProjects/ViaStellis

--- android/app/src/main/java/com/acalistudios/viastellis/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.acalistudios.viastellis;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.acalistudios.viastellis',
   appName: 'ViaStellis',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    path: 'C:/Users/hansi/AndroidStudioProjects/ViaStellis'
+  }
 };
 
 export default config;
